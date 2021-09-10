7ce024968c93e0525e855452ade94956ce627a31
kevbrain <44575942+kevbrain@users.noreply.github.com>
2021-09-10 15:36:33 +0200

    Update Application.java

--- spring-boot/src/main/java/com/its4u/Application.java
+++ spring-boot/src/main/java/com/its4u/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		 SpringApplication.run(Application.class, args);
-		 System.out.println("Deployed with GitOps Application Bootstrapper test 5");
+		 System.out.println("Deployed with GitOps Application Bootstrapper test 6");
 	}
 	
 }
